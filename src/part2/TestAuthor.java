0cb06315ce0c7d1ebcdcc378d0f736892c5d60f4
Loic HERNANDEZ <loic.hernandez@etu.univ-amu.fr>
2025-01-28 15:54:07 +0100

    Ajout de l'exercice 1 §2

---
+++ src/part2/TestAuthor.java
@@ -0,0 +1,13 @@
+package part2;
+
+public class TestAuthor {
+	public static void main(String[] arg) {
+		// Test constructeur & toString
+		Author author = new Author("Vives", "vives@author.com", 'm');
+		System.out.println(author);
+		
+		// Test Getter & Setter
+		author.setEmail("N/A");
+		System.out.println(author.getEmail());
+	}
+}
